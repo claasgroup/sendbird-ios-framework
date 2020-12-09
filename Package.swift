cf6fc58506b39ec0bdb0b44678060df8a4889464
Yannik Ehlert <yannik.ehlert@claas.com>
2020-12-09 16:11:33 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SendBirdSDK",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SendBirdSDK",
+            targets: ["SendBirdSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "SendBirdSDK", url: "https://www.claas.com", checksum: "0000000000000000000000000000000000000000000000000000000000000000"),
+    ]
+)
