d4098421dcf03ca62c66ae96fe42452ad095b604
Microsoft.TeamFoundation.System <>
2021-06-25 23:41:58 +0200

    SendBirdSDK v3.0.226

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "SendBirdSDK", url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.0.225/SendBirdSDK.xcframework.zip", checksum: "045b40bb4c0514a96aba0f1818d83d7b7bd332d4dcd416e4e19950a4b527e01d"),
+        .binaryTarget(name: "SendBirdSDK", url: "https://github.com/sendbird/sendbird-ios-framework/releases/download/v3.0.226/SendBirdSDK.xcframework.zip", checksum: "50beab2f328463ecd70a5dee8d4007b10ba06d451535839aad8a6364f1f4e855"),
     ]
 )
